4fd57e33f4409d0d64ae59345fc9e5090d15d4b3
caloy0714 <58654567+caloy0714@users.noreply.github.com>
2022-12-03 13:12:51 +0800

    Update factory_method1.php

--- factory_method1.php
+++ factory_method1.php
@@ -1,44 +1,20 @@
 <?php
-
-namespace RefactoringGuru\FactoryMethod\Conceptual;
-
-/**
- * The Creator class declares the factory method that is supposed to return an
- * object of a Product class. The Creator's subclasses usually provide the
- * implementation of this method.
- */
 abstract class Logistics
 {
-    /**
-     * Note that the Creator may also provide some default implementation of the
-     * factory method.
-     */
-    abstract public function PlanDelivery();
     abstract public function CreateTransport();
 
-    /**
-     * Also note that, despite its name, the Creator's primary responsibility is
-     * not creating products. Usually, it contains some core business logic that
-     * relies on Product objects, returned by the factory method. Subclasses can
-     * indirectly change that business logic by overriding the factory method
-     * and returning a different type of product from it.
-     */
-    public function someOperation(): string
+    public function PlanDelivery(): string
     {
         // Call the factory method to create a Product object.
-        $product = $this->factoryMethod();
+        $logis = $this->factoryMethod();
         // Now, use the product.
         $result = "Creator: The same creator's code has just worked with " .
-            $product->operation();
+            $logis->operation();
 
         return $result;
     }
 }
 
-/**
- * Concrete Creators override the factory method in order to change the
- * resulting product's type.
- */
 class RoadLogistics extends Logistics
 {
     public function CreateTransport()
@@ -46,12 +22,6 @@ class RoadLogistics extends Logistics
         return new Truck();
     }
 
-    public function PlanDelivery()
-    {
-        return "plan";
-    }
-
-    
 }
 
 class SeaLogistics extends Logistics
@@ -61,26 +31,15 @@ class SeaLogistics extends Logistics
         return new Boat();
     }
 
-    public function PlanDelivery()
-    {
-        return "plan";
-    }
 }
 
-/**
- * The Product interface declares the operations that all concrete products must
- * implement.
- */
 interface Transport 
 {
     public function deliver();
     public function load();
-    public function uload();
+    public function unload();
 }
 
-/**
- * Concrete Products provide various implementations of the Product interface.
- */
 class Truck implements Transport
 {
     public function deliver()
@@ -117,26 +76,18 @@ class Sea implements Transport
     }
 }
 
-/**
- * The client code works with an instance of a concrete creator, albeit through
- * its base interface. As long as the client keeps working with the creator via
- * the base interface, you can pass it any creator's subclass.
- */
-function clientCode(Creator $creator)
+
+function clientCode(Logistics $creator)
 {
     // ...
-    echo "Client: I'm not aware of the creator's class, but it still works.\n"
-        . $creator->someOperation();
+    echo "Client Only\n"
+        . $creator->PlanDelivery();
     // ...
 }
 
-/**
- * The Application picks a creator's type depending on the configuration or
- * environment.
- */
-// echo "App: Launched with the ConcreteCreator1.\n";
-// clientCode(new ConcreteCreator1());
-// echo "\n\n";
+echo "Launch Truck\n";
+clientCode(new truck());
+echo "\n\n";
 
-// echo "App: Launched with the ConcreteCreator2.\n";
-// clientCode(new ConcreteCreator2());
+echo "Launch Boat\n";
+clientCode(new boat());
